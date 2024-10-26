6733e57fde28f786fa8635c887c7537ec1ad3474
RNT13 <renatornt13@gmail.com>
2024-10-26 15:57:36 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,35 +1,62 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    ocultaElementosDoHeader();
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    });
     
+    // Seção de atrações, programação das abas //
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
-            const tabTarget = botao.target.dataset.tabButton;
+            const tabTarget = botao.currentTarget.dataset.tabButton;
             const tab = document.querySelector(`[data-tab-id="${tabTarget}"]`);
             hideAllTabs();
             tab.classList.add('shows__list--is-active');
             removeActive();
-            botao.target.classList.add('shows__tabs__button--is-active');
-        })
+            botao.currentTarget.classList.add('shows__tabs__button--is-active');
+        });
     }
 
+    // Seção FAQ, Accordion
     for (let i = 0; i < questions.length; i++) {
-        questions[i].addEventListener('click', abreOuFechaResposta)
+        questions[i].addEventListener('click', abreOuFechaResposta);
     }
-})
+});
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open';
-    const  elementoPai = elemento.target.parentNode;
+    const elementoPai = elemento.currentTarget.parentNode;
 
     elementoPai.classList.toggle(classe);
 }
 
-function removeActive(elemento) {
+function removeActive() {
     const buttons = document.querySelectorAll('[data-tab-button]');
 
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.remove('shows__tabs__button--is-active')
+        buttons[i].classList.remove('shows__tabs__button--is-active');
     }
 }
 
